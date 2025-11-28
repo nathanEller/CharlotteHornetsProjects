8df33f6ad67af89280289fcf249d894b8e56d17f
Nathan Eller <nathan.eller1@gmail.com>
2025-11-27 22:39:43 -0500

    adding player id function and working on get player stats function

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ main fuction
 import rostersAndPlayers
 
 def main():
-        df = rostersAndPlayers.charlotteRoster(2025)
+        #df = rostersAndPlayers.getRoster(2019)
+        #print(df)
+
+        #playerID = rostersAndPlayers.playerNameToID("Brandon Miller")
+        #print(playerID)
+
+        df = rostersAndPlayers.getPlayerStats("LaMelo Ball", "counting")
         print(df)
 
 if __name__ == "__main__":
--- rostersAndPlayers.py
+++ rostersAndPlayers.py
@@ -15,29 +15,48 @@ import pandas as pd
 import time
 
 #return a dataframe of the players on the Hornets for the season ending in year provided
-def charlotteRoster(year):
-	#give url and instruct selenium to have eager loading
+def getRoster(year):
+	#give url and instruct selenium to have eager loading to prevent timeout errors
 	url=(f'https://www.basketball-reference.com/teams/CHO/{year}.html')
 	options= webdriver.ChromeOptions()
 	options.page_load_strategy = 'eager'
 
-	#instantiate driver and make connection
+	#instantiate driver and make connection; sleeping 2 seconds to give elements time to load
 	driver=webdriver.Chrome(options=options)
-	driver.get(url) 
-
-	##5 seconds to give basic html elements time to load
+	driver.get(url)
 	time.sleep(2)
 
-	##page insepction shows that the element to inspect is a table with the id of 'roster'
+	#page insepction shows that the element to inspect is a table with the id of 'roster'
 	table = driver.find_element(By.ID, 'roster')
 	table_html = table.get_attribute('outerHTML')
-	
 	df = pd.read_html(table_html)[0]
 
 	#clean up connection
 	driver.quit()
 	return df
 
-#return player stats for a given year
-def playerStatsYear(playerName, year):
-	return 200
+#take a player name and return their idTag
+#player name will be passed as FirstName LastName (with space)
+# Format: (last initial)/(first 5 letters of last name)(first 2 letters of first name)(01, 02, etc.).
+def playerNameToID(playerName):
+	firstLast=playerName.lower().split() #returns a list of two strings, the first name followed by the last name
+	lastInitial=(firstLast[1])[0]
+
+	if len(firstLast[1]) < 5:
+		lastNameLetters = firstLast[1]
+	else:
+		lastNameLetters = firstLast[1][:5]
+
+	if len(firstLast[0]) < 2:
+		firstNameLetters = firstLast[0]
+	else:
+		firstNameLetters = firstLast[0][:2]
+	idTag=(f'{lastInitial}/{lastNameLetters}{firstNameLetters}01')#need to determine when 01 vs 02
+	return idTag
+
+#return dataframe of player counting stats or player advanced stats for their career
+#mode will be either "count" or "adv"
+def getPlayerStats(player, mode):
+	idTag = playerNameToID(player)
+	url=(f'https://www.basketball-reference.com/players/{idTag}.html')
+	return url
